1425900d2340860b63d708b06d6cb4008a094915
Alex Nederlof <alex@nederlof.com>
2013-12-05 21:09:24 +0100

    Fixed nmap script

--- nmap.sh
+++ nmap.sh
@@ -1,1 +1,5 @@
-sudo nmap -sn  192.168.1.63-253 -oX machines-online.xml > /dev/null && cat
+#!/bin/bash
+#Run in the scripts dir
+cd $(cd "$(dirname "$0")" && pwd -P)/..;
+
+sudo nmap -sn  192.168.1.63-253 -oX machines-online.xml > /dev/null
