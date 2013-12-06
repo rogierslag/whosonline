d56119bef102e4dbb6394449a233737b831710de
Alex Nederlof <alex@nederlof.com>
2013-12-06 07:27:04 +0100

    Fixed pointer to the wrong dir in script

--- nmap.sh
+++ nmap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #Run in the scripts dir
-cd $(cd "$(dirname "$0")" && pwd -P)/..;
+cd $(cd "$(dirname "$0")" && pwd -P)/;
 
 sudo nmap -sn  192.168.1.63-253 -oX machines-online.xml > /dev/null
